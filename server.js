08fba166e18b0bbdfe48aefd4f69b76d75d5d634
SaadiaELF <saadiaelfekak@gmail.com>
2022-12-31 18:47:13 +0000

    Added delete one booking route

--- server.js
+++ server.js
@@ -9,6 +9,13 @@ app.use(cors());
 //Use this array as your (in-memory) data store.
 const bookings = require("./bookings.json");
 
+// Functions
+function isInvalidId(id, index, response) {
+  if (index < 0) {
+    response.status(400).send("No booking with the Id '" + id + "' is found");
+  }
+}
+
 app.get("/", function (request, response) {
   response.send("Hotel booking server.  Ask for /bookings, etc.");
 });
@@ -28,6 +35,19 @@ app.get("/bookings/:id", function (request, response) {
       .status(400)
       .send("No booking with the Id '" + request.params.id + "' is found");
   }
+});
+
+// Delete one booking by id
+app.delete("/bookings/:id", function (request, response) {
+  let bookingIndex = bookings.findIndex((elt) => elt.id == request.params.id);
+
+  isInvalidId(request.params.id, bookingIndex, response);
+
+  bookings.splice(bookingIndex, 1);
+  response.status(200).json({
+    msg: "booking successfully deleted",
+    bookings: bookings,
+  });
 });
 
 const listener = app.listen(process.env.PORT, function () {
